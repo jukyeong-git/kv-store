83afa9785707962dd9cf94626377ad5a07908876
Ryan Jukyeong Kim <ryan.jukyeong.kim@gmail.com>
2026-04-05 23:01:03 +0800

    Initial commit

---
+++ src/test/java/io/github/jukyeong/kvstore/KvStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jukyeong.kvstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
