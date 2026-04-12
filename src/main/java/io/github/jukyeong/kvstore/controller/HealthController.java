d6dc3f8cc263937c13d6969d3cb889c8ef6b673d
Ryan Jukyeong Kim <ryan.jukyeong.kim@gmail.com>
2026-04-12 23:26:00 +0800

    feat: add HealthController to redirect root path to actuator health

---
+++ src/main/java/io/github/jukyeong/kvstore/controller/HealthController.java
@@ -0,0 +1,19 @@
+package io.github.jukyeong.kvstore.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Hidden
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public void redirectToHealth(
+            HttpServletResponse response) throws IOException {
+        response.sendRedirect("/actuator/health");
+    }
+}
